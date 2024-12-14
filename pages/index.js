738a64a9bbc551ef407651f01827a7a3d61c5f39
dev-joaovitor <joaotorvitor1@gmail.com>
2024-12-14 18:49:52 -0300

    bake your cake, we handle the hassle!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
 function Home() {
-  return <h1>Mica, i love you!</h1>;
+  return (
+    <article>
+      <h1>Bake your cake, we handle the hassle!</h1>
+
+      <p>
+        Possibly a management system and integrated store
+        for small bakeries.
+      </p>
+
+      <p>
+        They will have a way to track ingredient and tools usage,
+        calculate cost-per-product, store recipes, show their creations,
+        get customers, create a calendar to allow customers to
+        schedule orders on available days and choose if they want to
+        pick it up or have it delivered, have payment handled as they
+        like: upfront payment, 50% upfront payment or payment-after-delivery,
+        get reviews about their products and so on...
+      </p>
+    </article>
+  );
 }
 
 export default Home;
